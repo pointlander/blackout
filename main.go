15533facf406238d915f47764cdc5b8fbfed3576
Andrew Snodgrass <pointlander@gmail.com>
2024-03-01 11:25:27 -0600

    Resize test image

--- main.go
+++ main.go
@@ -4,6 +4,46 @@
 
 package main
 
-func main() {
+import (
+	"image"
+	_ "image/jpeg"
+	"image/png"
+	"log"
+	"os"
+	"strings"
 
+	"github.com/nfnt/resize"
+)
+
+func main() {
+	file, err := os.Open("test.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	name := info.Name()
+	name = name[:strings.Index(name, ".")]
+
+	input, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+
+	input = resize.Resize(32, 32, input, resize.NearestNeighbor)
+
+	file, err = os.Create(name + ".png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = png.Encode(file, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
 }
